079dd17f6ec765d89c1dc6796a7807b1392e94e9
victormaofeng <3286408344@qq.com>
2020-08-15 19:36:42 +0800

    mao:排查bug

--- src/main/java/com/dbsy/obe/aspect/LoginAspect.java
+++ src/main/java/com/dbsy/obe/aspect/LoginAspect.java
@@ -8,13 +8,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @Component
 @Aspect
